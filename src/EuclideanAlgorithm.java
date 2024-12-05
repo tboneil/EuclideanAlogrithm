76b5c7f60b985b5f2a0f7e1e3cf2ec689c62a1a2
Tyler O'Neil <78081823+activePurple@users.noreply.github.com>
2024-12-05 00:17:04 -0700

    Initial commit

---
+++ src/EuclideanAlgorithm.java
@@ -0,0 +1,22 @@
+// Simple Java program that performs Euclidean Algorithm to find GCD
+import java.util.Scanner;
+
+public class EuclideanAlgorithm {
+    public static int getGCD(int firstInt, int secondInt) {
+        if (secondInt == 0) {
+            return firstInt;
+        }
+
+        return getGCD(secondInt, firstInt % secondInt);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstInt = scanner.nextInt();
+        int secondInt = scanner.nextInt();
+        int result;
+
+        result = getGCD(firstInt, secondInt);
+        System.out.println("GCD: " + result);
+    }
+}
